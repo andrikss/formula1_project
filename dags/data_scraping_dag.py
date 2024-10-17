b7ef3510b8b3cfc08ed8a732d640b65cdd78d638
andrikss <andreaa.vasiljevic@gmail.com>
2024-10-17 17:37:45 +0200

    finished-data-scraping

--- dags/data_scraping_dag.py
+++ dags/data_scraping_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator  # Import BashOperator
+from airflow.operators.bash import BashOperator  
 from datetime import datetime
 
 default_args = {
